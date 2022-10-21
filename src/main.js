d7d16adc090b54d006243b780f23caa356ebc79e
Natalia Ruchs <naaty1xd@gmail.com>
2022-10-21 00:46:39 -0300

    add novas bandeiras

--- src/main.js
+++ src/main.js
@@ -7,9 +7,17 @@ const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
+    default: ["black", "gray"],
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
-    default: ["yellow", "gray"]
+    diners: ["#7f8fa6", "#40739e"],
+    amex: ["#192a56", "#7f8fa6"],
+    maestro: ["#273c75", "#e84118"],
+    cielo: ["#A99E46", "#D32E48"],
+    hipercard: ["#822124", "#A1585B"],
+    discover: ["#00A4E0", "#EF4123"],
+    jcb: ["#00823f", "#bb1933"],
+    unionpay: ["red", "red"]
   }
 
   ccBgColor01.setAttribute("fill", colors[type][0])
@@ -50,6 +58,10 @@ const cardNumberPattern = {
   mask: [
     {
       mask: "0000 0000 0000 0000",
+      cardtype: "default"
+    },
+    {
+      mask: "0000 0000 0000 0000",
       regex: /^4\d{0,15}/,
       cardtype: "visa"
     },
@@ -59,8 +71,45 @@ const cardNumberPattern = {
       cardtype: "mastercard"
     },
     {
+      mask: "0000 000000 0000",
+      regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+      cardtype: "diners"
+    },
+    {
       mask: "0000 0000 0000 0000",
-      cardtype: "default"
+      regex: /^3[47][0-9]{0,13}/,
+      cardtype: "amex"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: "maestro"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{0,2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/,
+      cardtype: "cielo"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^606282|^3841(?:[0|4|6]{0,1})0/,
+      cardtype: "hipercard"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+      cardType: "discover"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:35\d{0,2})\d{0,12}/,
+      cardType: "jcb"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^62\d{0,14}/,
+      cardtype: "unionpay"
     }
   ],
   dispatch: function (appended, dynamicMasked) {
